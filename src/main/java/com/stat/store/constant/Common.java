60fc9022c0a87c4fa860896d73f9569941e34904
long.nguyen <long.nguyen@c-mg.com>
2015-04-14 09:34:55 +0100

    update account login for Android

--- src/main/java/com/stat/store/constant/Common.java
+++ src/main/java/com/stat/store/constant/Common.java
@@ -6,6 +6,6 @@ package com.stat.store.constant;
 public class Common {
     public static final String PERSISTENCE_UNIT = "SSPU";
     public static final String ANDROID_ID = "3A2653A6F9D442E8";
-    public static final String USERNAME = "ececalikus00@gmail.com";
-    public static final String PASSWORD = "STd1uvz0";
+    public static final String USERNAME = "ececalikus@gmail.com";
+    public static final String PASSWORD = "ultraslan2";
 }
